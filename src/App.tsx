cd9041561fc9f6364e0c2e53dc713d82c8342b1d
vitorbelluzzo <vitorbelluzzo@hotmail.com>
2024-02-16 19:04:03 -0300

    Estrutura da aplicação - Estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { ThemeProvider } from "styled-components";
 import { Button } from "./components/button";
+
 import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
@@ -10,6 +12,11 @@ export function App() {
       <Button variant="danger" />
       <Button variant="success" />
       <Button />
+
+      asdasd
+
+
+      <GlobalStyle />
     </ThemeProvider>
   ) 
 

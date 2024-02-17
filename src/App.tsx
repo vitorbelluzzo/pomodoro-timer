c82244bd935ae30c1856ff0e2b92d033c62f5339
vitorbelluzzo <vitorbelluzzo@hotmail.com>
2024-02-17 17:58:58 -0300

    Páginas e rotas - React Router DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/button'
-
+import { BrowserRouter } from 'react-router-dom'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { Router } from './router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="danger" />
-      <Button variant="success" />
-      <Button />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )

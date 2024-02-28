88da4c4bae07205f5b578fceb6ebdca077c1f9f9
vitorbelluzzo <vitorbelluzzo@hotmail.com>
2024-02-27 23:41:52 -0300

    Páginas e rotas - Página: Home

--- src/layouts/defaultLayout/index.tsx
+++ src/layouts/defaultLayout/index.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom'
 import { Header } from '../../components/Header'
 import { LayoutContainer } from './styles'
+import { Home } from '../../pages/home'
 
 export function DefaultLayout() {
   return (
     <LayoutContainer>
       <Header />
-      <Outlet />
+      <Home />
     </LayoutContainer>
   )
 }

94f058370e9829c0d6dcc9b8cdf72ef5edec2cb6
vitorbelluzzo <vitorbelluzzo@hotmail.com>
2024-02-28 23:40:11 -0300

    feat: estilização do button

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,6 +4,7 @@ import {
   FormContainer,
   HomeContainer,
   Separator,
+  StartCountdownButton,
 } from './styles'
 
 export function Home() {
@@ -28,10 +29,10 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <button type="submit">
+        <StartCountdownButton type="submit">
           <Play size={24} />
           Começar
-        </button>
+        </StartCountdownButton>
       </form>
     </HomeContainer>
   )
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -50,3 +50,24 @@ export const Separator = styled.div`
   display: flex;
   justify-content: center;
 `
+
+export const StartCountdownButton = styled.button`
+  width: 100%;
+  border: 0;
+  padding: 1rem;
+  border-radius: 8px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  font-weight: bold;
+
+  cursor: pointer;
+
+  background: ${(props) => props.theme['red-500']};
+  color: ${(props) => props.theme['gray-100']};
+  &:hover {
+    background: ${(props) => props.theme['red-700']};
+  }
+`
